1d6c5040c5f821439a72022095296f40af17cbfb
Anderson <arobertok@gmail.com>
2022-02-10 21:05:03 -0300

    Implementação resource method

--- src/main/java/br/com/druidess/person/PersonResource.java
+++ src/main/java/br/com/druidess/person/PersonResource.java
@@ -1,16 +1,25 @@
 package br.com.druidess.person;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/people")
 public class PersonResource {
 
+  @Autowired
+  private RestTemplate restTemplate;
+
   @GetMapping("/name")
   public String getName() {
-    return "Eduardo";
+    String telefone = restTemplate.getForObject("http://dante-service/dante/phone", String.class);
+
+    return "Eduardo" + telefone;
   }
+
+
 
 }
